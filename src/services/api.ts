a4c0ab6bac8c7411a72d81d5df42695766bcd925
mateussf99 <mateus.ms65@gmail.com>
2025-09-16 13:53:37 -0300

    :sparkles: feat: integrando com a api pra fazer o login

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 // lembra de colocar no .env -> VITE_API_URL=http://localhost:5000
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_URL ;
 
 
 const api: AxiosInstance = axios.create({
