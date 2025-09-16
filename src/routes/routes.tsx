243a0927f2b77da6ef648780cfa81b9580289f07
mateussf99 <mateus.ms65@gmail.com>
2025-09-16 19:05:50 -0300

    :sparkles: feat: headers pronto integrados com o back na parte de login

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,26 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Header from '../components/header';
+import Header2 from '../components/header2';
 import '../index.css';
 import Home from '../pages/home';
 import Login from '../pages/login';
 import Informacoes from '../pages/informacoes';
+import { useAuth } from '@/contexts/AuthContext';
 
 
 
 function AppRoutes() {
+    const { isAuthenticated } = useAuth()
     return (
         
         <BrowserRouter>
             <div className="flex flex-col min-h-screen">
                 <div className='fixed top-0 left-0 w-full z-10'>
-                    <Header />
+                    {isAuthenticated ? <Header2 /> : <Header />}
                 </div>
                 
                 <div className='pt-10 flex-grow'>
                     <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/informacoes" element={<Informacoes />} />
-                        <Route path="/login" element={<Login />} />
+                            <Route path="/" element={<Home />} />
+                            <Route path="/informacoes" element={<Informacoes />} />
+                            <Route
+                                path="/login"
+                                element={
+                                    isAuthenticated ? <Navigate to="/" replace /> : <Login />
+                                }
+                            />
                     </Routes>
                 </div>
             </div>
